b2589d5e3488b32a53ea8905e4a74676ceb389a4
TrejGun <trejgun@gmail.com>
2024-09-28 19:11:38 +0400

    fix: eslint + ethberry

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ echo -e "\033[34mTesting...\n\033[0m";
 set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
 
 export NODE_ENV=test
-export GEMUNION_API_KEY=$GEMUNION_API_KEY
+export ETHBERRY_API_KEY=$ETHBERRY_API_KEY
 export AWS_REGION=$AWS_REGION
 export AWS_S3_BUCKET=$AWS_S3_BUCKET
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
@@ -14,7 +14,5 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 
 lerna bootstrap --hoist --ignore-scripts
 lerna run build --stream
+lerna run test --concurrency 1
 
-# lerna run lint --stream
-
-lerna exec -- npm run test
